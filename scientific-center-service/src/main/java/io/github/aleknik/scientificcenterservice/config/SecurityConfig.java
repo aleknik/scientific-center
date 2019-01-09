18a170dab5f5cff5777e05b0e5585d3510508926
Aleksandar Nikolic <nikolic95@gmail.com>
2019-01-09 22:03:06 +0100

    Add security config and base model

---
+++ scientific-center-service/src/main/java/io/github/aleknik/scientificcenterservice/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package io.github.aleknik.scientificcenterservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll();
+
+        httpSecurity.headers().frameOptions().disable();
+    }
+}
---
+++ scientific-center-service/src/main/java/io/github/aleknik/scientificcenterservice/model/BaseModel.java
@@ -0,0 +1,23 @@
+package io.github.aleknik.scientificcenterservice.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseModel {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    public BaseModel() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
