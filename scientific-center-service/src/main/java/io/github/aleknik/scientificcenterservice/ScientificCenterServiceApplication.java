e55291194167c14d18861c46f8fa7f828282fb82
Aleksandar Nikolic <nikolic95@gmail.com>
2019-01-09 18:12:40 +0100

    initial commit

---
+++ scientific-center-service/src/main/java/io/github/aleknik/scientificcenterservice/ScientificCenterServiceApplication.java
@@ -0,0 +1,14 @@
+package io.github.aleknik.scientificcenterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScientificCenterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScientificCenterServiceApplication.class, args);
+    }
+
+}
+
